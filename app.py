316b1c9e8442fcdf86a5695342faadb37349ff76
Jonsia Cejohn <jonsiacejohn@gmail.com>
2026-05-31 11:19:11 +0530

    Modify Flask app to use dynamic port and disable debug
    Updated the app to run on a dynamic port and disabled debug mode.

--- app.py
+++ app.py
@@ -98,4 +98,9 @@ def stats():
     })
 
 if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
